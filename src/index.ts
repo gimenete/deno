91cd4e208d6657de8156379a76f543a40a7ce543
Alberto Gimeno <gimenete@users.noreply.github.com>
2023-07-13 12:17:03 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import { serve } from "https://deno.land/std@0.176.0/http/server.ts";
 
 let port = parseInt(Deno.env.get("PORT") ?? "8000");
-const s = serve({ port });
-
-console.log(`http://localhost:${port}/`);
 
 console.log("EXAMPLE env variable:", Deno.env.get("EXAMPLE"));
-
-for await (const req of s) {
-	req.respond({ body: "Choo Choo! Welcome to your Deno app\n" });
-}
