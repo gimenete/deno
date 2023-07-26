1f3aabd39e1ba11be806583bc5c8212a6ab87e05
Alberto Gimeno <gimenete@gmail.com>
2023-07-26 11:40:14 +0200

    Add timeout

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { serve } from "https://deno.land/std@0.176.0/http/server.ts";
 
 let port = parseInt(Deno.env.get("PORT") ?? "8000");
 
-console.log("EXAMPLE env variable:", Deno.env.get("EXAMPLE"));
+console.log("EXAMPLE from railway CLI", Deno.env.get("EXAMPLE"));
+
+setTimeout(() => {
+  console.log("bye bye")
+}, 3000)
